023d757e3b34c514f37f38cb0ddabe35e30822a1
mohitlokhande27 <40323103+mohitlokhande27@users.noreply.github.com>
2018-06-23 15:02:11 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  /*template: `<h1> Welcome to Marvellous Infosystems! </h1> <h2>Educating for Better Tommorow</h2>`,*/
+  styles: []
+  
+})
+export class AppComponent {
+  title = 'FirstApp';
+}
+  
---
+++ src/app/marvellous-comp/marvellous-comp.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marvellous-comp',
+  templateUrl: './marvellous-comp.component.html',
+  styleUrls: ['./marvellous-comp.component.css']
+})
+export class MarvellousCompComponent implements OnInit {
+
+  public name="marvellous"
+  constructor() { }
+
+  ngOnInit() {
+  }
+  fun()
+  {
+    return " Inside Fun"
+  }
+
+}
